183d6614b93780243bd5c099c009c6ef5a4c03cd
James Prance <j.prance@gmx.com>
2017-04-17 14:05:53 +0100

    Added calculation of std deviation and median of data. Added some comments/documentation

--- parse.js
+++ parse.js
@@ -1,35 +1,97 @@
 const urlFile = 'urls.json'
 const outputDir = './output'
 
-const jfile = require('jsonfile');
+const jsonfile = require('jsonfile');
 const csv = require('csvtojson');
-const _= require('lodash');
+const _ = require('lodash');
+const math = require('mathjs');
 const fs = require('fs');
 const request = require('request');
 
-jfile.readFile(urlFile, 'utf8', function (err,obj) {
+jsonfile.readFile(urlFile, 'utf8', function (err,obj) {
   if (err) {
     return console.log(err);
   }
-
-  _.each(obj.inputFiles,downloadCsv);
+  _.each(obj.inputFiles,function(jsonurl){
+    downloadCsv(jsonurl, function(response){
+      csvToObj(response,function(){
+        calcMedianTemp();
+        calcStdDeviationTemp();
+      });
+    });
+  });
 
 });
 
-function downloadCsv(url) {
+/*
+ * Function to download CSV from url,
+ * Returns body of request
+ */
+function downloadCsv(url,cb) {
   console.log('Downloading ',url);
   request(url,function(error, response, body) {
     if (response.statusCode != 200) {
       console.log('Error:',error);
     } else {
-      csv()
-      .fromString(body)
-      .on('csv',(csvRow)=>{ // this func will be called 3 times
-          console.log(csvRow) // => [1,2,3] , [4,5,6]  , [7,8,9]
-      })
-      .on('done',()=>{
-          console.log('Finished Parsing')
-      })
+      cb(body);
     }
   });
+}
+
+/*
+ * Function to convert CSV to JS object
+ */
+function csvToObj(str,cb) {
+  var set = []
+  csv().fromString(str)
+  .on('json',(jsonObj, rowIndex)=>{ // called on each row
+      //console.log(jsonObj);
+      set.push(jsonObj);
+  })
+  .on('done',()=>{ // called when done
+      //console.log('Finished Parsing:');
+      //console.log(set);
+      this.set = set;
+      cb();
+  })
+}
+
+/*
+ * Function to map temperature data to array
+ * and calculate median
+ */
+function calcMedianTemp() {
+  var median = getMedian( _.map(this.set, function(x){ return x['Temperature']; }) );
+  //var median = getMedian( [1,2,3] );
+  //console.log(_.map(this.set,function(x){ return x['Temperature']; }));
+  console.log(median);
+}
+
+/*
+ * Calculate median from array
+ * https://github.com/Delapouite/lodash.math/blob/master/lodash.math.js
+ */
+function getMedian(arr) {
+  arr = arr.slice(0);
+  var middle = (arr.length + 1) / 2;
+  var sorted = math.sort(arr);
+  return (sorted.length % 2) ? sorted[middle - 1] : (sorted[middle - 1.5] + sorted[middle - 0.5]) / 2;
+}
+
+/*
+ * Function to map temperature data to array
+ * and calculate standard deviation
+ */
+function calcStdDeviationTemp() {
+  var stdDev = stdDeviation(_.map(this.set,function(x){ return x['Temperature']; }));
+  console.log(stdDev);
+}
+
+/*
+ * Calculate standard deviation from array
+ * https://gist.github.com/venning/b6593f965773985f923f
+ */
+function stdDeviation(arr){
+  var avg = _.sum(arr)/arr.length;
+  return math.sqrt(_.sum(_.map(arr, (i) => math.pow((i - avg), 2))) / arr.length);
 }
