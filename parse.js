f3975dbb9c2228e468a338e411f56c5720f5903d
James Prance <j.prance@gmx.com>
2017-04-17 17:10:08 +0100

    Added functionality to support calculation of number of days rotted and output to newline delimited json file

--- parse.js
+++ parse.js
@@ -1,5 +1,5 @@
 const urlFile = 'urls.json'
-const outputDir = './output'
+const outFile = 'output.json'
 
 const jsonfile = require('jsonfile');
 const csv = require('csvtojson');
@@ -7,16 +7,24 @@ const _ = require('lodash');
 const math = require('mathjs');
 const fs = require('fs');
 const request = require('request');
+const ndjson = require('ndjson');
+
+const tempThreshold = 30;
+const humidityThreshold = 65;
 
 jsonfile.readFile(urlFile, 'utf8', function (err,obj) {
   if (err) {
     return console.log(err);
   }
-  _.each(obj.inputFiles,function(jsonurl){
+  prepNDJSON();
+  _.each(obj.inputFiles,function(jsonurl,index){
     downloadCsv(jsonurl, function(response){
       csvToObj(response,function(){
-        calcMedianTemp();
-        calcStdDeviationTemp();
+        var med = calcMedianTemp();
+        var sd = calcStdDeviationTemp();
+        var rot = getRottingTime();
+        var jsonline = {"grainPile": index+1, "standardDeviation":sd, "median":med, "rotsAtTime":rot};
+        appendToNDJSON(jsonline);
       });
     });
   });
@@ -59,9 +67,8 @@ function csvToObj(str,cb) {
  */
 function calcMedianTemp() {
   var median = getMedian( _.map(this.set, function(x){ return x['Temperature']; }) );
-  //var median = getMedian( [1,2,3] );
-  //console.log(_.map(this.set,function(x){ return x['Temperature']; }));
-  console.log(median);
+  console.log('median:'+median);
+  return median;
 }
 
 /*
@@ -81,7 +88,8 @@ function getMedian(arr) {
  */
 function calcStdDeviationTemp() {
   var stdDev = stdDeviation( _.map(this.set,function(x){ return x['Temperature']; }) );
-  console.log('sd:'+stdDev);
+  console.log('stddev:'+stdDev);
+  return stdDev;
 }
 
 /*
@@ -99,4 +107,39 @@ function getVariance(arr){
 
 function mean(arr){
   return _.sum(arr.map(Number))/_.size(arr);
+}
+
+/*
+ * Function to find when the grain will rot
+ * The arrays are compared to threshold values set above
+ */
+function getRottingTime(){
+  var daysUntilHumidRot = _.findIndex(_.map(this.set, function(x){ return x['Humidity']; }), function(y) {return y >= humidityThreshold;});
+  var daysUntilTempRot = _.findIndex(_.map(this.set, function(x){ return x['Temperature']; }), function(y) {return y >= tempThreshold;});
+  var days = (daysUntilHumidRot > daysUntilTempRot) ? daysUntilTempRot : daysUntilHumidRot;
+  console.log('daysToRot:'+days);
+  return days;
+}
+
+/*
+ * Function which sets up the NDJSON library
+ * with a stream we can use for writing
+ */
+function prepNDJSON(){
+  this.serialize = ndjson.serialize();
+  this.serialize.on('data', function(line) {
+    fs.appendFile(outFile, line, function (err) {
+      if (err){
+        console.log('Error appending to outFile');
+      }
+    });
+  });
+}
+
+/*
+ * Function which uses the NDJSON stream
+ * for writing the output json file
+ */
+function appendToNDJSON(json){
+  this.serialize.write(json)
 }
